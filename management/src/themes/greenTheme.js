799e8b0cb19a97b95da524c2c10bc3048c916f6a
Kiran Behara <kiranbehara69@gmail.com>
2023-12-09 16:29:48 +0530

    ui fixes

---
+++ management/src/themes/greenTheme.js
@@ -0,0 +1,48 @@
+import { createTheme } from "@mui/material";
+
+const defaultGeneralSettings = {
+  fontSize: 12,
+  fontFamily: "sans-serif",
+};
+
+const greenTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#0e6f5d",
+    },
+    secondary: {
+      main: "#eaa342",
+    },
+    error: {
+      main: "#FF0000",
+    },
+    background: {
+      default: "#FFFBF5",
+      paper: "#FFFBF5",
+    },
+  },
+  typography: {
+    fontFamily: `${defaultGeneralSettings.fontFamily}`,
+    fontSize: defaultGeneralSettings.fontSize,
+  },
+  components: {
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          height: "30px",
+          minHeight: "30px",
+        },
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        sx: {
+          textTransform: "capitalize !important",
+          height: "30px",
+        },
+      },
+    },
+  },
+});
+export default greenTheme;
