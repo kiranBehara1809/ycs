cadaaa8f52984ae9f25b58e6eb57ca9b475d184d
Kiran Behara <kiranbehara69@gmail.com>
2024-03-20 11:23:24 +0530

    settings added

---
+++ src/db/dbMasters/settingsMenu.js
@@ -0,0 +1,16 @@
+import { PALETTE_ICON } from "../../constants/icons";
+
+const SETTINGS_MENU = [
+  {
+    name: "Theme",
+    url: null,
+    isDialog: true,
+    uniqueName: "THEME",
+    apiEndPoint: null,
+    openInNewTab: false,
+    icon: PALETTE_ICON,
+    hideAddBtn: true,
+  },
+];
+
+export { SETTINGS_MENU };
