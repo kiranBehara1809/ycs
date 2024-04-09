3296bacab9f213d67016df4bc3791d5fc0abfe4e
Kiran Behara <kiranbehara69@gmail.com>
2024-04-09 19:25:08 +0530

    del un co

--- src/constants/common.js
+++
@@ -1,7 +0,0 @@
-const MALE_OBJECT = { objId: "658422803d6e0eec81dc5d0f", label: "Male" };
-const FEMALE_OBJECT = { objId: "6584206e3d6e0eec81dc5cee", label: "Female" };
-const MISTER_OBJECT = { objId: "6586a827bdf10d100092ddb3", label: "MR" };
-const MISSES_OBJECT = { objId: "6586a86ebdf10d100092ddc1", label: "MRS" };
-const MISS_OBJECT = { objId: "6586a875bdf10d100092ddc4", label: "MS" };
-
-export { MALE_OBJECT, FEMALE_OBJECT, MISSES_OBJECT, MISS_OBJECT, MISTER_OBJECT };
